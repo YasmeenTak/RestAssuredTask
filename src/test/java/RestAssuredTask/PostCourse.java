e8b3f685adee1b8f78da45994cadc287c566bfc5
Yasmeen Tak <yasmeen.tak.33@gmail.com>
2021-11-09 14:58:23 +0200

    Post one course successflly

---
+++ src/test/java/RestAssuredTask/PostCourse.java
@@ -0,0 +1,42 @@
+package RestAssuredTask;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.response.Response;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.json.JSONObject;
+
+public class PostCourse {
+	// Test Case
+	@Test
+	static void postTestCase() {
+		JSONObject requestParams = new JSONObject();
+		requestParams.put("name", "SDLC");
+		requestParams.put("description", "Software Development Life");
+		requestParams.put("auther", "Yasmeeeen");
+
+		
+		
+		baseURI = "http://localhost:3000";
+		
+		RequestSpecification req = given();
+		req.header("content-type", "application/json");
+		req.body(requestParams.toString());
+		
+		Response res = req.post("/courses");
+		int statusCode = res.getStatusCode();
+		Assert.assertEquals(statusCode, 201);
+		
+		String courseName =res.jsonPath().get("name");
+		Assert.assertEquals(courseName, "SDLC");
+
+	}
+
+}
