38716b8814b92ab56c911bf626cba0f3b2b56b0d
Yasmeen Tak <yasmeen.tak.33@gmail.com>
2021-11-09 21:03:16 +0200

    Delete specfic course

---
+++ src/test/java/RestAssuredTask/DeleteCourse.java
@@ -0,0 +1,25 @@
+package RestAssuredTask;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class DeleteCourse {
+	@Test
+	static void DeleteCourseTestCase() {
+		baseURI = "http://localhost:3000";
+
+		RequestSpecification req = given();
+		req.header("content-type", "application/json");
+
+		Response res = req.delete("/courses/6");
+
+		int statusCode = res.getStatusCode();
+		Assert.assertEquals(statusCode, 200);
+	}
+
+}
